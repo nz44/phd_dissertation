cda4ff7873b025a08cf4ae68dd2839cc6c40addc
Naixin Zhu <naixin88@gmail.com>
2020-09-30 22:57:54 -0400

    create dataframe for plotting scatter by group

--- scripts/2_functions_data_visualization.py
+++ scripts/2_functions_data_visualization.py
@@ -7,8 +7,10 @@ import pandas as pd
 import datetime
 import os.path
 import ast
+import pickle
 
 graph_output = '/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/graphs'
+input_path = Path("/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____")
 
 #*****************************************************************************************************
 #*** INPUT DATA (scraped before 202009) is transformed to dataframe
@@ -110,11 +112,11 @@ def transform_old_scraper_dict_dataframe(raw_data, date_panel_scraped):
     E['check_all_genre_covered'] = E['GAME'] + E['UTILITIES'] + E['SOCIAL_MEDIA_LEISURE']
 
     # categorical dummies for content rating
-    E['contentRating_18+'] = np.where(E['contentRating'].str.contains("18+"), 1, 0)
-    E['contentRating_17+'] = np.where(E['contentRating'].str.contains("17+"), 1, 0)
-    E['contentRating_10+'] = np.where(E['contentRating'].str.contains("10+"), 1, 0)
-    E['contentRating_everyone'] = np.where(E['contentRating'].str.contains("Everyone"), 1, 0)
-    E['contentRating_teen'] = np.where(E['contentRating'].str.contains("Teen"), 1, 0)
+    E['contentRating_Adults only 18+'] = np.where(E['contentRating'].str.contains("18+"), 1, 0)
+    E['contentRating_Mature 17+'] = np.where(E['contentRating'].str.contains("17+"), 1, 0)
+    E['contentRating_Everyone 10+'] = np.where(E['contentRating'].str.contains("10+"), 1, 0)
+    E['contentRating_Everyone'] = np.where(E['contentRating'].str.contains("Everyone"), 1, 0)
+    E['contentRating_Teen'] = np.where(E['contentRating'].str.contains("Teen"), 1, 0)
 
     # a column is a pandas series, so this problem is creating pandas series from dictionary
     # Since it is in string format, so first turn string into dictionary, then extract the dictioanry values and turn them into a list
@@ -122,14 +124,16 @@ def transform_old_scraper_dict_dataframe(raw_data, date_panel_scraped):
     # note that unlike data scraped using the new scraper, they put the 5-star number of ratings in the first position
     # initial data or trakcing data scraped in 201907, 201908, 201912, 202001, 202002 do not have histogram, using
     # ast.literal_eval will raise value error, note str.contains is only a pandas series method
-    date_without_score_histogram = ['201907', '201908', '201912', '202001', '202002']
+    date_without_score_histogram = ['201907', '201908', '201909', '201912', '202001', '202002']
     if any(x in date_panel_scraped for x in date_without_score_histogram):
         E[['score_5', 'score_4', 'score_3', 'score_2', 'score_1']] = np.nan
+        E['ratings'] = np.nan
 
     else:
         X = E['histogram'].apply(lambda x: ast.literal_eval(x))
         Y = X.apply(lambda x: list(x.values()))
         E[['score_5', 'score_4', 'score_3', 'score_2', 'score_1']] = Y.apply(pd.Series)
+        E['ratings'] = E['score_5'] + E['score_4'] + E['score_3'] + E['score_2'] + E['score_1']
 
     # convert IAP price range into IAP price low or IAP price high -- skip the null
     # for 201812,it seems that if you use list(). the list would break every digit into an element, so I will use str.split into two columns here
@@ -211,9 +215,6 @@ def transform_dict_dataframe(raw_data, date_panel_scraped):
     E['developerAddress'] = E['developerAddress'].str.encode("ascii", "ignore").astype("string")
     E['comments'] = E['comments'].str.encode("ascii", "ignore").astype("string")
 
-    E['price'] = E['price'].str.replace('$', '')
-    E['price'] = E['price'].astype(float)
-
     # a column is a pandas series, so this problem is creating pandas series from list
     E[['score_1', 'score_2', 'score_3', 'score_4', 'score_5']] = E['histogram'].apply(pd.Series)
     #change column names with date suffix so that
@@ -226,7 +227,35 @@ def transform_dict_dataframe(raw_data, date_panel_scraped):
     print(E.dtypes)
     return(E)
 
+
+#*****************************************************************************************************
+#*** MERGE
+#*****************************************************************************************************
+
+def merge_dataframes_panels(initial_date, subsequent_date_list):
+
+    folder = initial_date + '_PANEL_DF'
+    f_name = 'INITIAL_' + initial_date + '.pickle'
+    q = input_path / "__PANELS__" / folder / f_name
+    with open(q, 'rb') as filename:
+        DF1 = pickle.load(filename)
+
+    panel_dfs = []
+    for i in subsequent_date_list:
+        f_name = i + '_PANEL.pickle'
+        q = input_path / "__PANELS__" / folder / f_name
+        with open(q, 'rb') as filename:
+            DF2 = pickle.load(filename)
+            panel_dfs.append(DF2)
+
+    new = DF1.join(panel_dfs[0], how = 'inner')
+    for j in range(1, len(panel_dfs), 1):
+        new = new.join(panel_dfs[j], how = 'inner')
+
+    return(new)
+
 ######################################################################################
+## plot single dataframe
 ######################################################################################
 def plot_dataframe(E, date_track_origin_date):
     # the input is the output of transform_dict_dataframe
@@ -275,3 +304,198 @@ def plot_dataframe(E, date_track_origin_date):
     f_name = date_track_origin_date + '_visualization.png'
     fig.savefig(os.path.join(graph_output, f_name),
                 facecolor='white', edgecolor='none', dpi=800)
+
+######################################################################################
+## plot panel trends
+# https://python-graph-gallery.com/124-spaghetti-plot/
+# https://matplotlib.org/3.1.0/gallery/subplots_axes_and_figures/broken_axis.html
+# https://jakevdp.github.io/PythonDataScienceHandbook/04.09-text-and-annotation.html
+# here, we are going to use nested functions
+######################################################################################
+
+# FUNCTION_1: Create a dataframe just for graphing
+# each column represent an app, and each row represent a time point (a panel)
+# the entire dataframe is about one variable: minInstalls
+
+def dataframe_for_line_plot(initial_date, panels, variable, **kwargs):
+
+    # open the merged panel dataframe
+    folder = initial_date + '_PANEL_DF'
+    df_name = initial_date + '_MERGED.pickle'
+    q = input_path / "__PANELS__" / folder / df_name
+    with open(q, 'rb') as filename:
+        B = pickle.load(filename)
+
+    # either randomly choose a subset or take the whole sample
+    if 'sample' in kwargs:
+        C = B.sample(kwargs['sample'])
+    else:
+        C = B
+
+    # select only the columns related to the variable
+    panels.insert(0, initial_date)
+    var_names = variable + '_'
+    col_names = list(map(lambda x: var_names + str(x), panels))
+    C = C[col_names]
+    for i in col_names:
+        C[i] = C[i].astype(float)
+
+    # figure out which apps have increase in the variable over the entire period
+    end_date = panels[-1]
+    end_date_var = variable + '_' + end_date
+    initial_date_var = variable + '_' + initial_date
+    C.loc[:, 'increase_over_interval'] = C.loc[:, end_date_var] - C.loc[:, initial_date_var]
+    C = C.sort_values(by='increase_over_interval', ascending=False)
+    n_rows = int(len(C.axes[0]))
+    if 'break_rule' in kwargs:
+        top_performers = C.loc[C['increase_over_interval'] > kwargs['break_rule']]
+        top_performers = top_performers.index.tolist()
+    elif n_rows >= 10:
+        top_performers = C.head(10)
+        top_performers = top_performers.index.tolist()
+    else:
+        top_performers = C
+        top_performers = top_performers.index.tolist()
+    C.drop('increase_over_interval', inplace=True, axis=1)
+    C = C.T
+
+    # create a column for graphing xticks (since you already modified panels, so you can skip adding the initial date)
+    time_axis = []
+    for j in panels:
+        time_axis.append(datetime.datetime.strptime(j, '%Y%m').strftime('%Y %b'))
+    C['x'] = time_axis
+
+    # take the title of app as the value of a dictionary for annotation in plotting
+    top_performers_dict = dict.fromkeys(top_performers)
+    title_column = 'title_' + initial_date
+    for app_id in top_performers_dict.keys():
+        top_performers_dict[app_id] = B.at[app_id, title_column]
+
+    return(C, top_performers_dict)
+
+###############################################################################################
+## LINE PLOTS
+# create new dataframe which each column holds the series of one app's data across panels
+###############################################################################################
+def graph_line_plots(C, initial_date, panels, variable, **kwargs):
+    fig = plt.figure(figsize=(11.69, 8.27))
+    plt.style.use('seaborn-paper')
+    spec = fig.add_gridspec(ncols=1, nrows=1)
+
+    # first subplot is the overall picture
+    ax1 = fig.add_subplot(spec[0, 0])
+    for column in C.drop('x', axis=1):
+        ax1.plot(C['x'], C[column], marker='', color='powderblue', linewidth=1, alpha=0.9, label=column)
+
+    # if top performers are specified, highlight those lines
+    annotate_pos = variable + '_202009'
+    if 'top_performers' in kwargs:
+        x_coord_frac = 0.95
+        y_coord_frac = 0.95
+        for app_id, app_title in kwargs['top_performers'].items():
+            ax1.plot(C['x'], C[app_id], marker='', color='deepskyblue', linewidth=2, alpha=0.7)
+            ax1.annotate(app_title,
+                        xy=('2020 Sep', C.at[annotate_pos, app_id]),
+                        xycoords='data',
+                        xytext=(x_coord_frac, y_coord_frac),
+                        textcoords='axes fraction',
+                        arrowprops={'arrowstyle': '->'},
+                        horizontalalignment='right',
+                        verticalalignment='bottom')
+            x_coord_frac += -0.08
+            y_coord_frac += -0.08
+
+    #ax1.grid(True)
+    ax1.set_xlabel('time')
+    string_initial_date = datetime.datetime.strptime(initial_date, '%Y%m').strftime('%Y %b')
+    string_end_date = datetime.datetime.strptime(panels[-1], '%Y%m').strftime('%Y %b')
+    if variable == 'minInstalls':
+        ax1.set_ylabel('cumulative minimum installs')
+        title = 'The Number of Minimum Installs of Apps from ' + string_initial_date + ' to ' + string_end_date
+        ax1.set_title(title)
+
+    # highlight the line that has the highest increase in min installs, and label them out in the legend
+    spec.tight_layout(fig)
+    f_name = initial_date + '_panels_' + variable + '_multiple_lines.png'
+    fig.savefig(os.path.join(graph_output, variable, f_name),
+                facecolor='white', edgecolor='none')
+
+    # second subplot zoom into the portion where the installs are lower and cramped together
+
+
+###############################################################################################
+## scatter plots (different color represents different category, and x-axis is the time)
+###############################################################################################
+def dataframe_for_scatter_plot_by_group(initial_date, panels, variable, group, **kwargs):
+    # open the merged panel dataframe
+    folder = initial_date + '_PANEL_DF'
+    df_name = initial_date + '_MERGED.pickle'
+    q = input_path / "__PANELS__" / folder / df_name
+    with open(q, 'rb') as filename:
+        B = pickle.load(filename)
+
+    # either randomly choose a subset or take the whole sample
+    if 'sample' in kwargs:
+        C = B.sample(kwargs['sample'])
+    else:
+        C = B
+
+    # select only the columns related to the variable, the panels and the group
+    panels.insert(0, initial_date)
+    var_names = variable + '_'
+    var_cols = list(map(lambda x: var_names + str(x), panels))
+    for i in var_cols:
+        C[i] = C[i].astype(float)
+    last_panel = panels[-1] # use the category specified in the most recent panel
+    group_cols = list(map(lambda x: str(x) + '_' + last_panel, group))
+    selected_cols = var_cols + group_cols
+    C = C[selected_cols]
+
+    # reshape the data with y-axis value in one column, x-axis value in another column (all panels stacked into one column)
+    # and categories in the third column
+    C['category'] = np.nan
+    for i in group:
+        col_name = str(i) + '_' + last_panel
+        C.loc[C[col_name] == 1, 'category'] = i
+
+    C['app_id'] = C.index
+    C.reset_index(drop=True, inplace=True)
+    D = pd.wide_to_long(C, stubnames='minInstalls', i='app_id', j='panel', sep='_')
+    D.reset_index(inplace=True)
+
+    return(D)
+
+
+
+# Plot
+# fig, ax = plt.subplots()
+# ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+# for name, group in groups:
+#     ax.plot(group.x, group.y, marker='o', linestyle='', ms=12, label=name)
+# ax.legend()
+
+
+###############################################################################################
+# def test_create_dataframe_for_plotting(initial_date, panels, variable, **kwargs):
+#
+#     # open the merged panel dataframe
+#     folder = initial_date + '_PANEL_DF'
+#     df_name = initial_date + '_MERGED.pickle'
+#     q = input_path / "__PANELS__" / folder / df_name
+#     with open(q, 'rb') as filename:
+#         B = pickle.load(filename)
+#
+#     # either randomly choose a subset or take the whole sample
+#     if 'sample' in kwargs:
+#         C = B.sample(kwargs['sample'])
+#     else:
+#         C = B
+#
+#     # select only the columns related to the variable
+#     panels.insert(0, initial_date)
+#     var_names = variable + '_'
+#     col_names = list(map(lambda x: var_names + str(x), panels))
+#     C = C[col_names]
+#     for i in col_names:
+#         C[i] = C[i].astype(float)
+#     return(C)
