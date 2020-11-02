42f8f49c057f2bd8365a23c3cd3ad7d5d98b0268
Naixin Zhu <naixin88@gmail.com>
2020-11-01 23:21:23 -0500

    spacy preprocessing

--- scripts/0_paths.py
+++ scripts/0_paths.py
@@ -7,12 +7,12 @@ from pathlib import Path
 # nlp_output = Path(r'C:\Users\naixi\OneDrive\__CODING__\PycharmProjects\GOOGLE_PLAY\nlp')
 
 # windows D drive path
-input_path = Path(r'D:\Dissertation Data Backup\2020 Oct 30\____WEB_SCRAPER____')
+# input_path = Path(r'D:\Dissertation Data Backup\2020 Oct 30\____WEB_SCRAPER____')
 
 # alienware linux computer path
-# input_path = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____')
-# output_path = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____/___cleaned_datasets___')
-# table_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/tables')
-# nlp_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/nlp')
+input_path = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____')
+output_path = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____/___cleaned_datasets___')
+table_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/tables')
+nlp_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/nlp')
 
 
--- scripts/4_natural_language_processing.py
+++ scripts/4_natural_language_processing.py
@@ -16,3 +16,42 @@ from spacy.lang.en.stop_words import STOP_WORDS
 stopwords = list(STOP_WORDS)
 print(stopwords)
 #spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+
+
+# ********************************************************************************************************
+### Preprocessing
+# ********************************************************************************************************
+
+def take_out_the_text_colume_from_merged_df(initial_panel, text_column_name):
+    folder_name = initial_panel + '_PANEL_DF'
+    f_name = initial_panel + '_MERGED.pickle'
+    q = input_path / '__PANELS__' / folder_name / f_name
+    with open(q, 'rb') as f:
+        D = pickle.load(f)
+
+    text_cols = D.filter(regex=text_column_name).columns
+    F =D[text_cols]
+    F.fillna('not available', inplace=True)
+
+    # below takes too long
+    for i in F.columns:
+        new_i = 'nlp' + i
+        F[new_i] = F[i].apply(lambda x: nlp(x))
+
+
+
+#### 1. Tokenization: break text into sentences and then into words, remove punctuations and stopwords
+
+
+
+#### 2. Lemmatization: for example, change third person verb to first person verb, change past tense to present tense
+
+
+#### 3. Stem: words are reduced to their root form
+
+
+
+
+# ********************************************************************************************************
+### Topic Modeling
+# ********************************************************************************************************
