e22e12bddabfc1ad3ec1ba969d787fa04710ddbe
Naixin Zhu <naixin88@gmail.com>
2020-11-29 23:24:52 -0500

    compared each row in each panel and assigned them niche vs broad, but unstable results

--- scripts/4_natural_language_processing.py
+++ scripts/4_natural_language_processing.py
@@ -146,7 +146,11 @@ def get_descriptor( init, k, tokenized_app_description, topic_index, top ):
 
 
 # embed try except inside the function you are going to apply to pandas
-def get_descriptor_in_tuple( init, k, tokenized_app_description, top ):
+# every time you run get_descriptor function, it will start from a new random initial position, which will
+# result in slightly different topic words.
+# so in order to keep consistency, you cannot run get_descriptor_in_tuple_or_list twice, once get tuple and once
+# get list, because this way you are actually running get_descriptor twice and get different topic words in tuple and list.
+def get_descriptor_in_tuple( init, k, tokenized_app_description, top):
     try:
         descriptors = []
         for topic_index in range(k):
@@ -160,16 +164,38 @@ def get_descriptor_in_tuple( init, k, tokenized_app_description, top ):
         print('error : ' + str_tokenized)
 
 
+def take_words_from_tuple(list_of_tuples):
+    a = []
+    if list_of_tuples is not None:
+        for tup in list_of_tuples:
+            # corresponding to [(1, 'marker, photo'), (2, 'design, instagram')]
+            if isinstance(tup[1], str):
+                x = tup[1].split(', ')
+                a.extend(x) # use extend bc x is a list of strings
+            # corresponding to [('game', 14420), ('app', 13258)]
+            elif isinstance(tup[0], str):
+                a.append(tup[0]) # use append bc tup[0] is just a string
+        # need to filter out duplicated topic words within each app's description
+        # this is good for later comparing them with the most frequently occurring topic words across all apps.
+        return list(set(a))
+    else:
+        return 'none' # to avoid 'float' type is not iterable in below row[i]
+
+
 def get_descriptor_for_each_row( initial_panel, panels_have_text, init, k, top, **kwargs ):
     df = open_token_df(initial_panel)
     # the columns to store the ranked topics
-    topic_cols = ['description_topics_' + item for item in panels_have_text]
-    for i in topic_cols:
+    topic_tuple_cols = ['topic_tuple_' + item for item in panels_have_text]
+    topic_list_cols = ['topic_list_' + item for item in panels_have_text]
+    for i in topic_tuple_cols:
+        df[i] = np.nan
+    for i in topic_list_cols:
         df[i] = np.nan
     if 'sample' in kwargs.keys():
         df = df.sample(n=kwargs['sample'])
     for i in panels_have_text:
-        df['description_topics_'+i] = df['description_'+i].progress_apply(lambda x: get_descriptor_in_tuple( init, k, x, top ))
+        df['topic_tuple_'+i] = df['description_'+i].progress_apply(lambda x: get_descriptor_in_tuple( init, k, x, top ))
+        df['topic_list_' + i] = df['topic_tuple_' + i].progress_apply(lambda x: take_words_from_tuple(x))
 
     # save
     folder_name = initial_panel + '_PANEL_DF'
@@ -191,23 +217,6 @@ def open_topic_df(initial_panel):
     df = pd.read_pickle(q)
     return df
 
-def take_words_from_tuple(list_of_tuples):
-    a = []
-    if list_of_tuples is not None:
-        for tup in list_of_tuples:
-            # corresponding to [(1, 'marker, photo'), (2, 'design, instagram')]
-            if isinstance(tup[1], str):
-                x = tup[1].split(', ')
-                a.extend(x) # use extend bc x is a list of strings
-            # corresponding to [('game', 14420), ('app', 13258)]
-            elif isinstance(tup[0], str):
-                a.append(tup[0]) # use append bc tup[0] is just a string
-        # need to filter out duplicated topic words within each app's description
-        # this is good for later comparing them with the most frequently occurring topic words across all apps.
-        return list(set(a))
-    else:
-        return 'none' # to avoid 'float' type is not iterable in below row[i]
-
 # https://towardsdatascience.com/lovecraft-with-natural-language-processing-part-2-tokenisation-and-word-counts-f970f6ff5690
 # bag-of-words: an unordered aggregated representation of a larger volume of text, which can be a document, chapter, paragraph, sentence, etc….
 # The grammar, punctuation, and word order from the original text are ignored, the only thing that’s kept is the
@@ -215,14 +224,10 @@ def take_words_from_tuple(list_of_tuples):
 # simply measuring whether the word is in the text or not.
 
 ## put all the topic words from relevent columns into a list (dict value) corresponding to panel names (dict key).
-def bag_of_words_of_combined_topic_words(initial_panel, panels_have_text):
+def bag_of_words(initial_panel, panels_have_text):
     df = open_topic_df(initial_panel)
-    str_topic_cols = ['str_description_topics_' + item for item in panels_have_text]
-    for i in str_topic_cols:
-        df[i] = np.nan
-    for i in panels_have_text:
-        df['str_description_topics_' + i] = df['description_topics_' + i].progress_apply(lambda x: take_words_from_tuple(x))
-    bow = dict.fromkeys(str_topic_cols, [])
+    topic_list_cols = ['topic_list_' + item for item in panels_have_text]
+    bow = dict.fromkeys(topic_list_cols, [])
     for i in bow.keys():
         for index, row in df.iterrows():
             bow[i].extend(row[i])
@@ -230,67 +235,79 @@ def bag_of_words_of_combined_topic_words(initial_panel, panels_have_text):
 
 ## count the frequency of a word appearing in a list (dict value) corresponding to panel names (dict key)
 def frequencies_bag_of_words(initial_panel, panels_have_text):
-    bow = bag_of_words_of_combined_topic_words(initial_panel, panels_have_text)
+    bow = bag_of_words(initial_panel, panels_have_text)
     fre_bow = dict.fromkeys(bow.keys())
     for k, v in bow.items():
         fre_bow[k] = Counter(v)
     return fre_bow
 
 # create a function that arbitrarily define a list of broad topic words for each panel
-def broad_topic_words(initial_panel, panels_have_text, threshold):
+def broad_or_niche_topic_words(initial_panel, panels_have_text, type, tup, threshold):
     fre_bow = frequencies_bag_of_words(initial_panel, panels_have_text)
-    l = dict.fromkeys(fre_bow.keys())
-    for k, v in fre_bow.items():
-        l[k] = []
-        for word, frequency in v.items():
-            if frequency >= threshold:
-                l[k].append(tuple((word, frequency)))
-        l[k] = sorted(l[k], key=lambda x: x[1], reverse=True)
-    return l
-
-# create a function that arbitrarily define a list of niche topic words for each panel
-def niche_topic_words(initial_panel, panels_have_text, threshold):
-    fre_bow = frequencies_bag_of_words(initial_panel, panels_have_text)
-    l = dict.fromkeys(fre_bow.keys())
-    for k, v in fre_bow.items():
-        l[k] = []
-        for word, frequency in v.items():
-            if frequency < threshold:
-                l[k].append(tuple((word, frequency)))
-        l[k] = sorted(l[k], key=lambda x: x[1])
-    return l
-
-# check if a column contains a word from a list of tuples
-# app_description_topic is from any cell of the column description_topic_panel after topic modeling
-# list_of_tuples is the value of any panels' broad words or niche words (from broad/niche topic_words functions)
-# so both input are list of tuples, the former one would be [(1, 'marker, photo'), (2, 'design, instagram')]
-# the latter one would be [('game', 14420), ('app', 13258)]
-def check_whether_an_apps_topic_word_contains_another_word(app_description_topic, list_of_tuples):
-    an_apps_topic_words = take_words_from_tuple(app_description_topic)
-    identifying_words = take_words_from_tuple(list_of_tuples)
-    return any(item in an_apps_topic_words for item in identifying_words)
+    if type == 'broad':
+        if tup is True:
+            l = dict.fromkeys(fre_bow.keys())
+            for k, v in fre_bow.items():
+                l[k] = []
+                for word, frequency in v.items():
+                    if frequency >= threshold:
+                        l[k].append(tuple((word, frequency)))
+                l[k] = sorted(l[k], key=lambda x: x[1], reverse=True)
+            return l
+        else:
+            l = dict.fromkeys(fre_bow.keys())
+            for k, v in fre_bow.items():
+                l[k] = []
+                for word, frequency in v.items():
+                    if frequency >= threshold:
+                        l[k].append(word)
+            return l
+    if type == 'niche':
+        if tup is True:
+            l = dict.fromkeys(fre_bow.keys())
+            for k, v in fre_bow.items():
+                l[k] = []
+                for word, frequency in v.items():
+                    if frequency < threshold:
+                        l[k].append(tuple((word, frequency)))
+                l[k] = sorted(l[k], key=lambda x: x[1], reverse=True)
+            return l
+        else:
+            l = dict.fromkeys(fre_bow.keys())
+            for k, v in fre_bow.items():
+                l[k] = []
+                for word, frequency in v.items():
+                    if frequency < threshold:
+                        l[k].append(word)
+            return l
 
 
 ## compare each app's topic words with a list of top topic words (above an arbitrary threshold, for example, >= 1000)
 ## each app has 6 topic words for now (they are not unique), first turn them
-def df_classify_app_into_broad_or_niche(initial_panel, panels_have_text, threshold_broad, threshold_niche):
-    broad_words = broad_topic_words(initial_panel, panels_have_text, threshold_broad)
-    niche_words = niche_topic_words(initial_panel, panels_have_text, threshold_niche)
+## classifying niche and broad, you do not need to do it for every year because we assume this is time-invariant feature
+def df_classify_app_into_broad_or_niche(initial_panel, the_panel, threshold_broad, threshold_niche, **kwargs):
+    broad_words = broad_or_niche_topic_words(initial_panel, the_panel, type = 'broad', tup = False, threshold = threshold_broad)
+    niche_words = broad_or_niche_topic_words(initial_panel, the_panel, type = 'niche', tup = False, threshold = threshold_niche)
     df = open_topic_df(initial_panel)
-    broad_cols = ['broad_app_' + item for item in panels_have_text]
+    if 'sample' in kwargs.keys():
+        df = df.sample(n=kwargs['sample'])
+    broad_cols = ['broad_app_' + item for item in the_panel]
     for i in broad_cols:
         df[i] = np.nan
-    niche_cols = ['niche_app_' + item for item in panels_have_text]
+    niche_cols = ['niche_app_' + item for item in the_panel]
     for i in niche_cols:
         df[i] = np.nan
 
-    for i in panels_have_text:
-        for index, row in df.iterrows():
-            if check_whether_an_apps_topic_word_contains_another_word(row['description_topics_'+i], broad_words['str_description_topics_' + i]):
-                row['broad_app_'+i] = 1
-            if check_whether_an_apps_topic_word_contains_another_word(row['description_topics_'+i], niche_words['str_description_topics_' + i]):
-                row['niche_app_'+i] = 1
-
+    for i in the_panel:
+        with tqdm(total=df.shape[0]) as pbar:
+            for index, row in df.iterrows():
+                pbar.update(1)
+                if any(item in row['topic_list_'+i] for item in broad_words['topic_list_'+i]):
+                    df.at[index, 'broad_app_'+i] = 1
+                if any(item in row['topic_list_'+i] for item in niche_words['topic_list_'+i]):
+                    df.at[index, 'niche_app_'+i] = 1
+    for i in the_panel:
+        df.fillna({'broad_app_'+i: 0, 'niche_app_'+i: 0}, inplace=True)
     # save
     folder_name = initial_panel + '_PANEL_DF'
     f_name = 'niche_broad_classified.pkl'
@@ -298,4 +315,10 @@ def df_classify_app_into_broad_or_niche(initial_panel, panels_have_text, thresho
     df.to_pickle(q)
     return df
 
-
+# ************************************************************************************************************
+def open_niche_classified_df(initial_panel):
+    folder_name = initial_panel + '_PANEL_DF'
+    f_name = 'niche_broad_classified.pkl'
+    q = input_path / '__PANELS__' / folder_name / f_name
+    df = pd.read_pickle(q)
+    return df
