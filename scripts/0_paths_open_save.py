e61d6a5b22c9c7f4d4124e302eb81231febe4a35
Naixin Zhu <naixin88@gmail.com>
2021-01-04 00:26:08 -0500

    organize code into classes

---
+++ scripts/0_paths_open_save.py
@@ -0,0 +1,109 @@
+from pathlib import Path
+import pandas as pd
+import warnings
+warnings.simplefilter('ignore')
+import pickle
+
+# ********************************************************************************************************
+# ********************************************************************************************************
+class Paths:
+    def __init__(self, initial_panel):
+        self.initial_panel = initial_panel
+# ********************************************************************************************************
+
+class Mac_Paths(Paths):
+    def __init__(self, initial_panel):
+        super().__init__(initial_panel)
+        self.input_path = Path('/Users/zhunaixin/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____')
+        self.output_path = Path('/Users/zhunaixin/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____/___cleaned_datasets___')
+        self.table_output = Path('/Users/zhunaixin/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/tables')
+        self.graph_output = Path('/Users/zhunaixin/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/graphs')
+        self.nlp_output = Path('/Users/zhunaixin/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/nlp')
+
+class Win_Paths(Paths):
+    def __init__(self, initial_panel):
+        super().__init__(initial_panel)
+        self.input_path = Path(r'C:\Users\naixi\OneDrive\_____GWU_ECON_PHD_____\___Dissertation___\____WEB_SCRAPER____')
+        self.output_path = Path(r'C:\Users\naixi\OneDrive\_____GWU_ECON_PHD_____\___Dissertation___\____WEB_SCRAPER____\___cleaned_datasets___')
+        self.table_output = Path(r'C:\Users\naixi\OneDrive\__CODING__\PycharmProjects\GOOGLE_PLAY\tables')
+        self.graph_output = Path(r'C:\Users\naixi\OneDrive\__CODING__\PycharmProjects\GOOGLE_PLAY\graphs')
+        self.nlp_output = Path(r'C:\Users\naixi\OneDrive\__CODING__\PycharmProjects\GOOGLE_PLAY\nlp')
+
+class Linux_Paths(Paths):
+    def __init__(self, initial_panel):
+        super().__init__(initial_panel)
+        self.input_path = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____')
+        self.output_path = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/_____GWU_ECON_PHD_____/___Dissertation___/____WEB_SCRAPER____/___cleaned_datasets___')
+        self.table_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/tables')
+        self.graph_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/nlp')
+        self.nlp_output = Path('/home/naixin/Insync/naixin88@sina.cn/OneDrive/__CODING__/PycharmProjects/GOOGLE_PLAY/graphs')
+
+# ********************************************************************************************************
+# ********************************************************************************************************
+class open_files(Linux_Paths):
+    def __init__(self, initial_panel, cluster_type=None, current_panel=None, tracking_panels=None):
+        self.cluster_type = cluster_type
+        self.current_panel = current_panel
+        self.tracking_panels = tracking_panels
+        super().__init__(initial_panel)
+
+    def open_merged_df(self):
+        folder_name = self.initial_panel + '_PANEL_DF'
+        f_name = self.initial_panel + '_MERGED.pickle'
+        q = self.input_path / '__PANELS__' / folder_name / f_name
+        df = pd.read_pickle(q)
+        return df
+
+    def open_token_df(self):
+        folder_name = self.initial_panel + '_PANEL_DF'
+        f_name = 'description_converted_to_spacy_tokens.pkl'
+        q = self.input_path / '__PANELS__' / folder_name / f_name
+        df = pd.read_pickle(q)
+        return df
+
+    def open_topic_df(self):
+        folder_name = self.initial_panel + '_PANEL_DF'
+        f_name = 'description_tokens_converted_to_topics.pkl'
+        q = self.input_path / '__PANELS__' / folder_name / f_name
+        df = pd.read_pickle(q)
+        return df
+
+    def open_cluster_df(self):
+        folder_name = self.initial_panel + '_PANEL_DF'
+        if self.cluster_type == 'k-means':
+            f_name = 'df_with_k_means_labels.pkl'
+        elif self.cluster_type == 'fuzzy-c-means':
+            f_name = 'df_with_fuzzy_c_means_labels.pkl'
+        q = self.input_path / '__PANELS__' / folder_name / f_name
+        df = pd.read_pickle(q)
+        return df
+
+    def open_df_to_id_for_scraping(self):
+        filename = "ALL_APP_DETAILS_" + self.initial_panel + '.pickle'
+        q = self.input_path / "NEW_ALGORITHM_MONTHLY_SCRAPE" / self.initial_panel / filename
+        df = pd.read_pickle(q)
+        return df
+
+    def open_app_detail_dict(self):
+        dfs = dict.fromkeys(self.tracking_panels)
+        for i in self.tracking_panels:
+            filename = 'TRACKING_' + i + '.pickle'
+            q = self.input_path / "TRACKING_THE_SAME_ID_MONTHLY_SCRAPE" / self.current_panel / filename
+            with open(q, 'rb') as f:
+                df = pickle.load(f)
+            dfs[i] = df
+        return dfs
+
+
+
+class save_files(Linux_Paths):
+
+    def __init__(self, initial_panel, current_panel, app_details_dict):
+        super().__init__(initial_panel)
+        self.current_panel = current_panel
+        self.app_details_dict = app_details_dict
+
+    def save_scraped_app_details(self):
+        filename = 'TRACKING_' + self.initial_panel + '.pickle'
+        q = self.input_path / "TRACKING_THE_SAME_ID_MONTHLY_SCRAPE" / self.current_panel / filename
+        pickle.dump(self.app_details_dict, open(q, 'wb'))
---
+++ scripts/1_scraping_cleaning_merge.py
@@ -0,0 +1,258 @@
+from google_play_scraper import app
+from tqdm import tqdm
+import numpy as np
+import random
+
+# convert the data scraped before 202009 from list of dictionaries to a dictionary with app ids as keys
+# some of them uses app_id as key and some of them uses appId as key
+# from 202009 onwards, the key conversion happend in scraping stage
+def convert_list_data_to_dict_with_appid_keys(C):
+    new_data = {}
+    for i in C:
+        try:
+            new_data[i['app_id']] = i
+        except:
+            new_data[i['appId']] = i
+    return(new_data)
+
+
+# get ID list
+# data scraped before 2020 Sep are organized in a list, each dictionary inside the list contains attributes and their values
+# for the data scraped in 2020 Sep and onwards, they are organized in dictionary with key as appid, and then their appdetails,
+# so id_list should just be C.keys()
+def get_id_from_data_before_202009(C):
+    id_list = []
+    for i in C:
+        if 'appId' in i.keys():
+            id_list.append(i['appId'])
+        else:
+            id_list.append(i['app_id'])
+    return(id_list)
+
+
+# scraping using google_play_scraper app function
+# the input is from get_id_from_old_data
+def scraping_apps_according_to_id(id_list):
+    app_details = dict.fromkeys(id_list)
+    # print(app_details)
+    for j in tqdm(range(len(id_list)), desc="scraping..."):
+        try:
+            app_details[id_list[j]] = app(id_list[j])
+        except:
+            pass
+    return(app_details)
+
+
+class app_detail_dicts:
+    # this app_detail_dict is opened from open_files class open_app_details_dict method
+    def __init__(self, app_details_dict):
+        self.app_details_dict = app_details_dict
+
+    def get_a_glimpse(self):
+        for k, v in self.app_details_dict.items():
+            print('PANEL', k)
+            random_app_id = random.choice(v.keys())
+            print('a random app id is:', random_app_id)
+            the_app_detail = v[random_app_id]
+            for feature, content in the_app_detail.items():
+                print(feature, " : ", content)
+
+    def transform_old_scraper_dict_dataframe(self):
+        # c are panels scraped before 202009
+        D = pd.DataFrame.from_dict(raw_data)
+        E = D.T
+        E.rename(columns={"category": "genreId", "description_html": "descriptionHTML",
+                          "recent_changes": "recentChanges", "iap": "offersIAP",
+                          "developer_id": "developerId", "current_version": "version",
+                          "required_android_version": "androidVersion",
+                          "content_rating": "contentRating", "iap_range": "inAppProductPrice",
+                          "developer_email": "developerEmail", "developer_url": "developerWebsite",
+                          "developer_address": "developerAddress", "app_id": "appId"}, inplace=True)
+
+        E.drop(['editors_choice', 'interactive_elements',
+                'descriptionHTML', 'recentChanges'], axis=1, inplace=True)
+
+        E = pd.get_dummies(E, columns=['free', 'offersIAP'], dtype=int)
+
+        # create new minInstalls column
+        E["minInstalls"] = E['installs'].str.replace('[^\w\s]', '')
+        E['minInstalls'] = E['minInstalls'].astype(float)
+
+        # change data type in columns, the old scraper did not include summary and comments columns
+        E['title'] = E['title'].str.encode("ascii", "ignore").astype("string")
+        E['description'] = E['description'].str.encode("ascii", "ignore").astype("string")
+        # E['summary'] = E['summary'].str.encode("ascii", "ignore").astype("string")
+        E['developer'] = E['developer'].str.encode("ascii", "ignore").astype("string")
+        E['developerId'] = E['developerId'].str.encode("ascii", "ignore").astype("string")
+        E['developerEmail'] = E['developerEmail'].str.encode("ascii", "ignore").astype("string")
+        E['developerWebsite'] = E['developerWebsite'].str.encode("ascii", "ignore").astype("string")
+        E['developerAddress'] = E['developerAddress'].str.encode("ascii", "ignore").astype("string")
+        # E['comments'] = E['comments'].str.encode("ascii", "ignore").astype("string")
+
+        # remove the list brackets in certain columns
+        # E['genreId'] = E['genreId'].apply(pd.Series)
+
+        E['GAME'] = np.where(E['genreId'].str.contains("GAME"), 1, 0)
+        utils = ['MEDICAL', 'EDUCATION', 'HEALTH_AND_FITNESS',
+                 'PARENTING', 'LIBRARIES_AND_DEMO',
+                 'HOUSE_AND_HOME', 'FINANCE', 'WEATHER', 'BOOKS_AND_REFERENCE',
+                 'TOOLS', 'BUSINESS', 'PRODUCTIVITY', 'TRAVEL_AND_LOCAL',
+                 'MAPS_AND_NAVIGATION', 'AUTO_AND_VEHICLES']
+        E['UTILITIES'] = np.where(E['genreId'].str.contains('|'.join(utils)), 1, 0)
+
+        social_media_l = ['COMICS', 'BEAUTY', 'ART_AND_DESIGN', 'DATING',
+                          'VIDEO_PLAYERS', 'SPORTS', 'LIFESTYLE',
+                          'PERSONALIZATION', 'SHOPPING', 'FOOD_AND_DRINK',
+                          'MUSIC_AND_AUDIO',
+                          'NEWS_AND_MAGAZINES', 'PHOTOGRAPHY', 'COMMUNICATION',
+                          'ENTERTAINMENT', 'SOCIAL', 'EVENTS']
+        E['SOCIAL_MEDIA_LEISURE'] = np.where(E['genreId'].str.contains('|'.join(social_media_l)), 1, 0)
+
+        # remove duplicate groups categories
+        # E.loc[(E.UTILITIES == 1 & ), 'Event'] = 'Hip-Hop'
+        E['UTILITIES'] = np.where(((E.GAME == 1) & (E.UTILITIES == 1)), 0, E.UTILITIES)
+        E['SOCIAL_MEDIA_LEISURE'] = np.where(((E.GAME == 1) & (E.SOCIAL_MEDIA_LEISURE == 1)), 0, E.SOCIAL_MEDIA_LEISURE)
+        # because the conflicts mostly occur where family education is in the same category as entertainment or art and design
+        E['SOCIAL_MEDIA_LEISURE'] = np.where(((E.UTILITIES == 1) & (E.SOCIAL_MEDIA_LEISURE == 1)), 0,
+                                             E.SOCIAL_MEDIA_LEISURE)
+
+        E['check_all_genre_covered'] = E['GAME'] + E['UTILITIES'] + E['SOCIAL_MEDIA_LEISURE']
+
+        # categorical dummies for content rating
+        E['contentRating_Adults only 18+'] = np.where(E['contentRating'].str.contains("18+"), 1, 0)
+        E['contentRating_Mature 17+'] = np.where(E['contentRating'].str.contains("17+"), 1, 0)
+        E['contentRating_Everyone 10+'] = np.where(E['contentRating'].str.contains("10+"), 1, 0)
+        E['contentRating_Everyone'] = np.where(E['contentRating'].str.contains("Everyone"), 1, 0)
+        E['contentRating_Teen'] = np.where(E['contentRating'].str.contains("Teen"), 1, 0)
+
+        # a column is a pandas series, so this problem is creating pandas series from dictionary
+        # Since it is in string format, so first turn string into dictionary, then extract the dictioanry values and turn them into a list
+        # now I can use pd.Series to break this list into several columns
+        # note that unlike data scraped using the new scraper, they put the 5-star number of ratings in the first position
+        # initial data or trakcing data scraped in 201907, 201908, 201912, 202001, 202002 do not have histogram, using
+        # ast.literal_eval will raise value error, note str.contains is only a pandas series method
+        date_without_score_histogram = ['201907', '201908', '201909', '201912', '202001', '202002']
+        if any(x in date_panel_scraped for x in date_without_score_histogram):
+            E[['score_5', 'score_4', 'score_3', 'score_2', 'score_1']] = np.nan
+            E['ratings'] = np.nan
+
+        else:
+            X = E['histogram'].apply(lambda x: ast.literal_eval(x))
+            Y = X.apply(lambda x: list(x.values()))
+            E[['score_5', 'score_4', 'score_3', 'score_2', 'score_1']] = Y.apply(pd.Series)
+            E['ratings'] = E['score_5'] + E['score_4'] + E['score_3'] + E['score_2'] + E['score_1']
+
+        # convert IAP price range into IAP price low or IAP price high -- skip the null
+        # for 201812,it seems that if you use list(). the list would break every digit into an element, so I will use str.split into two columns here
+        # however, for 2019 and onwards, the inAppProductPrice is already in list, so apply pd.Series worked
+        # in Both cases, they have some weird contents, mostly from content rating, in IAPprice column, I do not know why. I just removed all letters [a-z]
+
+        if '201812' in date_panel_scraped:
+            E['inAppProductPrice'] = np.where(E['inAppProductPrice'].str.contains('[a-z]', regex=True), np.nan,
+                                              E.inAppProductPrice)
+            E['inAppProductPrice'] = E['inAppProductPrice'].str.replace('$', '')
+            E['inAppProductPrice'] = E['inAppProductPrice'].str.replace('(', '')
+            E['inAppProductPrice'] = E['inAppProductPrice'].str.replace(')', '')
+            E['inAppProductPrice'] = E['inAppProductPrice'].str.replace("'", '')
+            E[['IAP_low', 'IAP_high']] = E['inAppProductPrice'].str.split(",", n=2, expand=True)
+            E[['IAP_low', 'IAP_high']] = E[['IAP_low', 'IAP_high']].astype(float)
+
+        else:
+            E[['IAP_low', 'IAP_high']] = E['inAppProductPrice'].apply(lambda x: pd.Series(x))
+            E['IAP_low'] = E['IAP_low'].str.replace('$', '')
+            E['IAP_high'] = E['IAP_high'].str.replace('$', '')
+            E['IAP_low'] = np.where(E['IAP_low'].str.contains('[a-z]', regex=True), np.nan,
+                                    E.IAP_low)
+            E['IAP_high'] = np.where(E['IAP_high'].str.contains('[a-z]', regex=True), np.nan,
+                                     E.IAP_high)
+            E[['IAP_low', 'IAP_high']] = E[['IAP_low', 'IAP_high']].astype(float)
+
+        E['price'] = E['price'].str.replace('$', '')
+        E['price'] = E['price'].astype(float)
+
+        # change column names with date suffix so that
+        E = E.add_suffix('_' + date_panel_scraped)
+        # fill none with nan, and remove rows that contain nan in ALL columns
+        E = E.fillna(value=np.nan)
+        E = E.dropna(axis=0, how="all")
+
+        return (E)
+
+    def transform_dict_dataframe(raw_data, date_panel_scraped):
+        # the raw data is scraped by 1_functions_scraping_data.py function scraping_apps_according_to_id
+        D = pd.DataFrame.from_dict(raw_data)
+        E = D.T
+        # E['updated_datetime'] = pd.to_datetime(E['updated'], origin='unix')
+        E['released_datetime'] = pd.to_datetime(E['released']).dt.date
+        E['today_datetime'] = datetime.date.today()
+        E['days_since_released'] = (E['today_datetime'] - E['released_datetime']).dt.days
+        E = pd.get_dummies(E, columns=['free', 'offersIAP',
+                                       'adSupported', 'containsAds', 'contentRating'], dtype=int)
+        E['GAME'] = np.where(E['genreId'].str.contains("GAME"), 1, 0)
+        E['UTILITIES'] = np.where(np.isin(E['genreId'], ['MEDICAL', 'EDUCATION', 'HEALTH_AND_FITNESS',
+                                                         'PARENTING', 'LIBRARIES_AND_DEMO', 'BOOKS_AND_REFERENCE',
+                                                         'HOUSE_AND_HOME', 'FINANCE', 'WEATHER',
+                                                         'TOOLS', 'BUSINESS', 'PRODUCTIVITY', 'TRAVEL_AND_LOCAL',
+                                                         'MAPS_AND_NAVIGATION', 'AUTO_AND_VEHICLES']), 1, 0)
+        E['SOCIAL_MEDIA_LEISURE'] = np.where(np.isin(E['genreId'], ['COMICS', 'BEAUTY', 'ART_AND_DESIGN', 'DATING',
+                                                                    'VIDEO_PLAYERS', 'SPORTS', 'LIFESTYLE',
+                                                                    'PERSONALIZATION', 'SHOPPING', 'FOOD_AND_DRINK',
+                                                                    'MUSIC_AND_AUDIO',
+                                                                    'NEWS_AND_MAGAZINES', 'PHOTOGRAPHY',
+                                                                    'COMMUNICATION',
+                                                                    'ENTERTAINMENT', 'SOCIAL', 'EVENTS']), 1, 0)
+        E['check_all_genre_covered'] = E['GAME'] + E['UTILITIES'] + E['SOCIAL_MEDIA_LEISURE']
+        # print("Non missing values in each column ", E.count())
+
+        # drop columns that will not be needed
+        E.drop(['descriptionHTML', 'summaryHTML', 'contentRatingDescription',
+                'recentChanges'], axis=1, inplace=True)
+
+        # first remove none ascii characters from the string then convert object to string type
+        E['title'] = E['title'].str.encode("ascii", "ignore").astype("string")
+        E['description'] = E['description'].str.encode("ascii", "ignore").astype("string")
+        E['summary'] = E['summary'].str.encode("ascii", "ignore").astype("string")
+        E['developer'] = E['developer'].str.encode("ascii", "ignore").astype("string")
+        E['developerId'] = E['developerId'].str.encode("ascii", "ignore").astype("string")
+        E['developerEmail'] = E['developerEmail'].str.encode("ascii", "ignore").astype("string")
+        E['developerWebsite'] = E['developerWebsite'].str.encode("ascii", "ignore").astype("string")
+        E['developerAddress'] = E['developerAddress'].str.encode("ascii", "ignore").astype("string")
+        E['comments'] = E['comments'].str.encode("ascii", "ignore").astype("string")
+
+        # a column is a pandas series, so this problem is creating pandas series from list
+        E[['score_1', 'score_2', 'score_3', 'score_4', 'score_5']] = E['histogram'].apply(pd.Series)
+        # change column names with date suffix so that
+        E = E.add_suffix('_' + date_panel_scraped)
+        # fill none with nan, and remove rows that contain nan in ALL columns
+        E = E.fillna(value=np.nan)
+        E = E.dropna(axis=0, how="all")
+        # check
+        print("after dropping rows all NA in all columns, the non-missing values in each column ", E.count())
+        print(E.dtypes)
+        return (E)
+
+    def merge_dataframes_panels(initial_date, subsequent_date_list):
+
+        folder = initial_date + '_PANEL_DF'
+        f_name = 'INITIAL_' + initial_date + '.pickle'
+        q = input_path / "__PANELS__" / folder / f_name
+        with open(q, 'rb') as filename:
+            DF1 = pickle.load(filename)
+
+        panel_dfs = []
+        for i in subsequent_date_list:
+            f_name = i + '_PANEL.pickle'
+            q = input_path / "__PANELS__" / folder / f_name
+            with open(q, 'rb') as filename:
+                DF2 = pickle.load(filename)
+                panel_dfs.append(DF2)
+
+        new = DF1.join(panel_dfs[0], how='inner')
+        for j in range(1, len(panel_dfs), 1):
+            new = new.join(panel_dfs[j], how='inner')
+
+        return (new)
+
+
+
+
